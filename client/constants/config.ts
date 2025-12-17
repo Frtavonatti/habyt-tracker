859095c82d9a05a8aeaae712eaa01ddc9f1c9e38
frtavonatti <frtavonatti@gmail.com>
2025-12-17 18:02:35 +0000

    feat: reintroduce getApiUrl function in config

--- client/constants/config.ts
+++ client/constants/config.ts
@@ -23,3 +23,33 @@ export const config = {
     (Constants.expoConfig?.extra?.apiBaseUrl as string) ?? 
     getDefaultApiUrl(),
 }
+
+/* function getApiUrl(): string {
+  if (Platform.OS === 'web') {
+    return 'http://localhost:3000/api'
+  }
+
+  // 2. For mobile attempt to use Expo web server id
+  const hostUri = Constants.expoConfig?.hostUri
+  if (hostUri) {
+    const host = hostUri.split(':')[0]
+    return `http://${host}:3000/api`
+  }
+
+  // 3. Env variables fallback 
+  const envUrl = Constants.expoConfig?.extra?.apiBaseUrl as string | undefined
+  if (envUrl) {
+    return envUrl
+  }
+
+  // 4. Last resource
+  return Platform.OS === 'android' 
+    ? 'http://10.0.2.2:3000/api'
+    : 'http://localhost:3000/api'
+}
+
+export const config = {
+  apiBaseUrl: getApiUrl()
+}
+
+console.log('🚀 API URL:', config.apiBaseUrl, '| Platform:', Platform.OS) */
